5666ab8b17d1f809acb50cb3c2370db739691dc7
monk-codes <your_donotreply_email_id>
2024-07-25 18:09:23 +0530

    setting middleware

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
  path: "./env",
 });
-connectDB();
+connectDB()
+ .then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+   console.log("Database connected successfully");
+  });
+ })
+ .catch((err) => {
+  console.error(`Error connecting to the database: ${err.message}`);
+  process.exit(1);
+ });
